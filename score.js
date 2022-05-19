e9947faa57a2e4edf62f03eed73b08b9c1990b33
EverythingIsTakenSkyz <85395037+EverythingIsTakenSkyz@users.noreply.github.com>
2022-05-19 16:20:28 +0200

    Add files via upload

---
+++ score.js
@@ -0,0 +1,38 @@
+//Antallet af coins
+
+class Score extends GameObject {
+
+    constructor () {
+        let drawOrder = 9;
+        super(drawOrder);
+        this.image = new Image(60, 60);
+        this.image.src = "../assets/images/coin.png";
+        this.imageXPosition = 70;
+        this.imageYPosition = 70;
+        this.textXPosition = 100;
+        this.textYPosition = 90;
+        this.textSize = 50;
+        this.textColor = "yellow";
+        this.value = 0;
+    }
+
+    draw() {
+        drawImage(this.image,
+            this.imageXPosition,
+            this.imageYPosition,
+            this.image.width,
+            this.image.height
+        );
+        drawText(
+            "x"+ this.value, //Viser hvor meget score du har (this.value)
+            this.textXPosition,
+            this.textYPosition,
+            this.textSize,
+            this.textColor
+        );
+    }
+
+    addPoints (points) {
+        this.value += points;
+    }
+}
